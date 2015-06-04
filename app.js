00aea8064c82712a191725f92f468b71edce5d16
Cheri <avyishi@gmail.com>
2015-06-04 01:08:23 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = angular.module('cruzzit', []);
+
+app.controller('MainCtrl', [
+'$scope',
+function($scope){
+  $scope.posts = [
+    {title: 'post 1', upvotes: 8},
+    {title: 'post 2', upvotes: 4},
+    {title: 'post 3', upvotes: 12},
+    {title: 'post 4', upvotes: 6},
+    {title: 'post 5', upvotes: 2}
+];
+
+  $scope.addPost = function(){
+    if(!$scope.title || $scope.title === '') { return;}
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
+  };
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
+
+}]);
