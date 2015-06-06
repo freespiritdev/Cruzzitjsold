7735332d9f4e2aa2bd12e04b0d81a19e0e0e6d39
Cheri <avyishi@gmail.com>
2015-06-05 20:23:53 -0700

    Added ability to allow users to post new comments

--- app.js
+++ app.js
@@ -64,4 +64,14 @@ function($scope, $stateParams, posts){
     ]
   });
 
+  $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
+
 }]);
